607e413139fc5a95e3b3af6412ccc17bb57d3331
Ricardo Avila <ravila@protonmail.com>
2020-11-15 20:35:56 -0800

    Fetch gene ids from mygene.info

--- parser.py
+++ parser.py
@@ -2,15 +2,16 @@ import glob
 import os
 import requests
 import urllib.parse
+import logging
 
-from biothings.utils.dataload import tabfile_feeder, dict_sweep, unlist
+from biothings.utils.dataload import tabfile_feeder
 import mygene
 
 
 def load_data(data_folder):
     # Load .gmt (Gene Matrix Transposed) files
     for f in glob.glob(os.path.join(data_folder, "*.gmt")):
-        # Get species name from the filename
+        # Get species name from the filename and convert to taxid
         species = f.replace(".gmt", "").split("-")[-1].replace("_", " ")
         taxid = get_taxid(species)
         data = tabfile_feeder(f, header=0)
@@ -23,7 +24,7 @@ def load_data(data_folder):
             # Get URL and gene list
             url = rec[1]
             ncbigenes = rec[2:]
-            genes = [{"ncbigene": gene} for gene in ncbigenes]
+            genes = fetch_gene_ids(ncbigenes, taxid)
             # Format schema
             doc = {'_id': wikipathways_id,
                    'is_public': True,
@@ -47,6 +48,34 @@ def get_taxid(species):
         data = response.json()
         taxid = data['hits'][0]['taxid']
         return taxid
+
+
+def fetch_gene_ids(ncbi_ids, taxid):
+    """Given entrez (ncbi) ids, fetch ensembl, gene symbol, name and _id."""
+    mg = mygene.MyGeneInfo()
+    fields = "ensembl.gene,symbol,name"
+    response = mg.querymany(ncbi_ids, scopes='entrezgene',
+                            species=taxid, fields=fields, returnall=True)
+    genes = []
+    for out in response['out']:
+        query = out['query']
+        if out.get('notfound'):
+            logging.warn("NCBI ids with no hits may be deprecated, skipping."
+                         .format(query))
+            continue
+        gene = {'mygene_id': out['_id'],
+                'ncbigene': query,
+                'symbol': out['symbol'],
+                'name': out['name']
+                }
+        if out.get('ensembl') is not None:
+            if len(out['ensembl']) > 1:
+                for i in out['ensembl']:
+                    gene.setdefault('ensemblgene', []).append(i['gene'])
+            else:
+                gene['ensemblgene'] = out['ensembl']['gene']
+        genes.append(gene)
+    return genes
 
 
 if __name__ == "__main__":
