2bc586ea6d2cf1169124250140d3735283f6aadb
Ricardo Avila <ravila@protonmail.com>
2020-11-19 17:36:18 -0700

    Add __metadata__

--- upload.py
+++ upload.py
@@ -5,6 +5,13 @@ from .parser import load_data
 class WikiPathwaysUploader(uploader.BaseSourceUploader):
 
     name = "wikipathways"
+    __metadata__ = {
+        "src_meta": {
+            'license_url': 'https://www.wikipathways.org/index.php/WikiPathways:License_Terms',
+            'licence': 'CC0 1.0 Universal',
+            'url': 'https://www.wikipathways.org/'
+            }
+        }
 
     def load_data(self, data_folder):
         self.logger.info("Load data from folder '%s'" % data_folder)
