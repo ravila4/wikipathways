665c5b4a306d601e222ad9d375aeae1c4978449f
Ricardo Avila <ravila@protonmail.com>
2020-12-08 13:51:08 -0700

    Move module to utils folder in mygeneset.info repo

--- parser.py
+++ parser.py
@@ -2,7 +2,7 @@ import glob
 import os
 
 from biothings.utils.dataload import tabfile_feeder
-from .geneset_utils import IDLookup
+from utils.geneset_utils import IDLookup
 
 
 def load_data(data_folder):
@@ -52,8 +52,8 @@ def load_data(data_folder):
             ncbigenes = rec[2:]
             genes = []
             for g in ncbigenes:
-                if converter.query_cache.get(g):
-                    genes.append(converter.query_cache[g])
+                if lookup.query_cache.get(g):
+                    genes.append(lookup.query_cache[g])
 
             # Format schema
             doc = {'_id': wikipathways_id,
