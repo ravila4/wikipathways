fbca77de9f779e63294c6914c8beb959a08675ee
Ricardo Avila <ravila@protonmail.com>
2021-01-15 16:33:35 -0800

    Add hardcoded mapping

--- upload.py
+++ upload.py
@@ -17,3 +17,65 @@ class WikiPathwaysUploader(uploader.BaseSourceUploader):
         self.logger.info("Load data from folder '%s'" % data_folder)
         wikipathways_docs = load_data(data_folder)
         return wikipathways_docs
+
+    @classmethod
+    def get_mapping(cls):
+        mapping = {
+            "is_public": {
+                "type": "boolean"
+            },
+            "taxid": {
+                "type": "integer"
+            },
+            "genes": {
+                "properties": {
+                    "mygene_id": {
+                        "normalizer": "keyword_lowercase_normalizer",
+                        "type": "keyword"
+                    },
+                    "symbol": {
+                        "type": "text"
+                    },
+                    "ncbigene": {
+                        "normalizer": "keyword_lowercase_normalizer",
+                        "type": "keyword"
+                    },
+                    "ensemblgene": {
+                        "normalizer": "keyword_lowercase_normalizer",
+                        "type": "keyword"
+                    },
+                    "uniprot": {
+                        "normalizer": "keyword_lowercase_normalizer",
+                        "type": "keyword"
+                    },
+                    "locus_tag": {
+                        "normalizer": "keyword_lowercase_normalizer",
+                        "type": "keyword"
+                    },
+                    "name": {
+                        "type": "text"
+                    }
+                }
+            },
+            "wikipathways": {
+                "properties": {
+                    "id": {
+                        "normalizer": "keyword_lowercase_normalizer",
+                        "type": "keyword",
+                        "copy_to": [
+                            "all"
+                        ]
+                    },
+                    "pathway_name": {
+                        "type": "text",
+                        "copy_to": [
+                            "all"
+                        ]
+                    },
+                    "url": {
+                        "normalizer": "keyword_lowercase_normalizer",
+                        "type": "keyword"
+                    }
+                }
+            }
+        }
